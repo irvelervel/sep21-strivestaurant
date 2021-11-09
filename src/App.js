aefc8b2d7fb94523f493ce3884b534e53be55b31
Stefano Casasola <ste.casasola@gmail.com>
2021-11-09 10:55:19 +0100

    Add files via upload
    Second break, started the Home component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import MyNavbar from './components/MyNavbar'
+import Home from './components/Home'
 
 function App() {
   return (
     <div>
-      <MyNavbar />
+      <MyNavbar brand="Strivestaurant" />
+      <Home />
     </div>
   )
 }
--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -1,26 +1,17 @@
-import { Navbar, Nav, NavDropdown } from 'react-bootstrap'
+// choose one way to import the two components from 'react-bootstrap
+// import { Navbar, Nav } from 'react-bootstrap'
+import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav'
 
-const MyNavbar = () => (
+const MyNavbar = (props) => (
     <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">{props.brand}</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="mr-auto">
-                <Nav.Link href="#features">Features</Nav.Link>
-                <Nav.Link href="#pricing">Pricing</Nav.Link>
-                <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                    <NavDropdown.Divider />
-                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                </NavDropdown>
-            </Nav>
-            <Nav>
-                <Nav.Link href="#deets">More deets</Nav.Link>
-                <Nav.Link eventKey={2} href="#memes">
-                    Dank memes
-                </Nav.Link>
+            <Nav className="ml-auto">
+                <Nav.Link href="#features">Menu</Nav.Link>
+                <Nav.Link href="#pricing">Reservation</Nav.Link>
+                <Nav.Link href="#pricing">Contact</Nav.Link>
             </Nav>
         </Navbar.Collapse>
     </Navbar>
